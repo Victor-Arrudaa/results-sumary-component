ee5f1755a6ed7bb532143c1869df4fc38cea774c
Victor Arruda <victor0070000@gmail.com>
2023-08-14 08:47:38 -0300

    adding display of items on screen from API

---
+++ js/index.js
@@ -0,0 +1,33 @@
+const itens = document.querySelector(".itens");
+
+async function getItens() {
+    const response = await fetch("/data.json");
+    const data = response.json();
+
+    return data;
+}
+
+const item = getItens().then((res) => {
+    res.forEach((element) => {
+        itens.innerHTML += createItem(element);
+    });
+});
+
+function createItem(item) {
+    let selectedItem = `
+    <div class="item ${item.category}">
+        <div class="text">
+        <img
+            src="${item.icon}"
+        />
+            <p>${item.category}</p>
+        </div>
+        <div class="result-one">
+            <p>${item.score}</p>
+
+            <span>/ 100</span>
+        </div>
+    </div>`;
+
+    return selectedItem;
+}
